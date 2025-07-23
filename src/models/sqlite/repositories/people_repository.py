c0f8f04c88fb9547d98c9acf08119b708f6fbe30
Thiago Miziara <thiago.miziara@luizalabs.com>
2025-07-23 11:41:07 -0300

    feat: implementing people_repository

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm.exc import NoResultFound
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
+
+
+class PeopleRepository:
+    def __init__(self, db_connection):
+        self.__db_connection = db_connection
+
+    def insert_person(
+        self, first_name: str, last_name: str, age: int, pet_id: int
+    ) -> None:
+        with self.__db_connection as db:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
+                )
+
+                db.session.add(person_data)
+                db.session.commit()
+            except Exception as execption:
+                db.session.rollback()
+                raise execption
+
+    def get_person(self, person_id: int) -> PeopleTable:
+        with self.__db_connection as db:
+            try:
+                person = (
+                    db.session.query(PeopleTable)
+                    .outerjoin(PetsTable, PeopleTable.pet_id == PetsTable.id)
+                    .filter(PeopleTable.id == person_id)
+                    .with_entities(
+                        PeopleTable.first_name,
+                        PeopleTable.last_name,
+                        PetsTable.name.label("pet_name"),
+                        PetsTable.type.label("pet_type"),
+                    )
+                    .one()
+                )
+
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
-from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.settings.conection import db_connection_handler
+from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 # db_connection_handler.conect_to_db()
 
@@ -16,5 +17,25 @@ def test_list_pets():
 def test_delete_pet():
     name = "belinha"
     repository = PetsRepository(db_connection_handler)
-    # Assuming there is a pet named 'Fido' in the database
+
     repository.delete_pets(name)
+
+
+@pytest.mark.skip(reason="interactive database test")
+def test_insert_person():
+    first_name = "John"
+    last_name = "Doe"
+    age = 30
+    pet_id = 1
+    repository = PeopleRepository(db_connection_handler)
+    repository.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="interactive database test")
+def test_get_person():
+    person_id = 1
+
+    repository = PeopleRepository(db_connection_handler)
+    response = repository.get_person(person_id)
+    print()
+    print(response)
