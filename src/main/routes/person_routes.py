1ca5e91404076f26be0a5e972e00eb4e19db65fa
Thiago Miziara <thiago.miziara@luizalabs.com>
2025-07-29 12:51:36 -0300

    feat: implementing error_handle

--- src/main/routes/person_routes.py
+++ src/main/routes/person_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from src.errors.error_handler import handle_errors
 from src.main.composer.person_finder_composer import person_finder_composer
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.person_creator_componser import person_creator_composer
@@ -8,19 +9,27 @@ person_routes_bp = Blueprint("person_routes", __name__)
 
 @person_routes_bp.route("/people", methods=["POST"])
 def create_person():
-    http_request = HttpRequest(body=request.json)
-    view = person_creator_composer()
+    try:
+        http_request = HttpRequest(body=request.json)
+        view = person_creator_composer()
 
-    http_response = view.handle(http_request)
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 
 @person_routes_bp.route("/people/<person_id>", methods=["GET"])
 def find_person(person_id):
-    http_request = HttpRequest(param={"person_id": person_id})
-    view = person_finder_composer()
+    try:
+        http_request = HttpRequest(param={"person_id": person_id})
+        view = person_finder_composer()
 
-    http_response = view.handle(http_request)
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
